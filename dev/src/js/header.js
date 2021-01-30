b36c8e9a315cc87909e982eca4398660edb2d5c0
Artem Avakyan <artem@aprint.pro>
2021-01-30 23:31:20 +0300

    Добавил все страницы и доделал меню

--- dev/src/js/header.js
+++ dev/src/js/header.js
@@ -1,13 +1,13 @@
 const $header = $(".header");
 let scrollPrev = 0;
 const cls = "header--scrolled";
-// const $sumenus = $header.find(".submenu__list");
+const $sumenus = $header.find(".dropdown__list");
 
 $(window).on("scroll", function () {
   const scrolled = $(window).scrollTop();
-  // const anyOpenSubmenu = $sumenus.css("visibility") === "visible";
+  const $anyOpenSubmenu = $sumenus.hasClass("show");
 
-  // if (anyOpenSubmenu) return;
+  if ($anyOpenSubmenu) return;
   if (scrolled > 50 && scrolled > scrollPrev) {
     $header.addClass(cls);
   } else {
